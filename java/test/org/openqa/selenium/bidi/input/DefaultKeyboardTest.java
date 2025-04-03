250a509a6ccff0ef83abfbf9fdfd209aa2539006
titusfortner <titus.fortner@gmail.com>
2025-04-02 18:31:04 -0700

    [rb] removing guards from Java tests; chrome bug fixed in v137

--- java/test/org/openqa/selenium/bidi/input/DefaultKeyboardTest.java
+++ java/test/org/openqa/selenium/bidi/input/DefaultKeyboardTest.java
@@ -229,12 +229,6 @@ class DefaultKeyboardTest extends JupiterTestBase {
 
   @Test
   @Ignore(IE)
-  @NotYetImplemented(
-      value = CHROME,
-      reason = "https://github.com/GoogleChromeLabs/chromium-bidi/issues/2321")
-  @NotYetImplemented(
-      value = EDGE,
-      reason = "https://github.com/GoogleChromeLabs/chromium-bidi/issues/2321")
   public void testSelectionSelectByWord() {
     assumeFalse(getEffectivePlatform(driver).is(Platform.MAC), "MacOS has alternative keyboard");
 
